7494d48ff9f03139df9ba0b4a6dbc80e754796ed
Mauri Keränen <maurikeranen@gmail.com>
2021-03-17 14:36:50 +0200

    3.9 - 3.11

---
+++ Osa 3/README.md
@@ -0,0 +1,2 @@
+Frontend:	http://rocky-cove-15430.herokuapp.com/
+Backend:	http://rocky-cove-15430.herokuapp.com/api/persons
--- Osa 3/index.js
+++ Osa 3/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('person', function (req, res) {
 	if (req.method === 'POST') {
