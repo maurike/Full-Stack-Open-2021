d05cc10d11b72a0078999784f594e9bdc1982d92
Mauri Keränen <maurikeranen@gmail.com>
2021-03-24 15:05:55 +0200

    5.1 - 5.22

--- Osa 4/blogilista/app.js
+++ Osa 4/blogilista/app.js
@@ -13,18 +13,18 @@ const mongoose = require('mongoose')
 logger.info('Connecting to', config.MONGODB_URI)
 
 mongoose
-	.connect(config.MONGODB_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false,
-		useCreateIndex: true
-	})
-	.then(() => {
-		logger.info('Connected to MongoDB')
-	})
-	.catch((error) => {
-		logger.error('Error connecting to MongoDB:', error.message)
-	})
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.static('build'))
@@ -35,6 +35,11 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', middleware.userExtractor, blogRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testRouter = require('./controllers/tests')
+  app.use('/api/testing', testRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
